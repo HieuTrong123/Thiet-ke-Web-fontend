b4a9a676efaddefb4413b1c40e3e1a724778c883
HieuTrong123 <87361973+HieuTrong123@users.noreply.github.com>
2023-03-10 13:54:49 +0700

    fontend

--- Testing/product/animation.js
+++
@@ -1,6 +0,0 @@
-function Model_On(){
-    document.querySelector(".model").classList.add("model__on");
-}
-function Model_Off(){
-    document.querySelector(".model").classList.remove("model__on");
-}
